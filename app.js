8d4b79ef588bdd9d239a8bf2686d662dbbe86b08
Stewart <stew.m.craig@gmail.com>
2017-11-27 14:24:02 +1100

    Add landing page
    Add landing page (index.html) that contains details of
    service and instructions for using service.

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const routes = require('./controllers/index');
+const path = require('path');
 require('./models/db');
 
 const app = express();
 const port = process.env.PORT || 8080;
 
+app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 
 app.listen(port);
